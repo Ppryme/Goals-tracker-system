34d4d6741a554287cc8305ae284c050a77fda94b
Ppryme <famuyiwapeter2004@gmail.com>
2026-05-27 13:29:46 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -58,8 +58,8 @@ app.post('/send-notification', async (req, res) => {
   res.json({ message: 'Notifications sent', results })
 })
 
-// 1:29pm daily cron
-cron.schedule('29 13 * * *', async () => {
+// 1:39pm daily cron
+cron.schedule('39 13 * * *', async () => {
   const subscriptions = await Subscription.find()
   const payload = JSON.stringify({
     title: 'Embark on your goals for daily excellence',
